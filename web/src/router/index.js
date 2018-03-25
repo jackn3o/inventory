6eeed7ee8c5d92a8a25eb85cc821d3bf27bc8a87
jackn3o <jackn3o@gmail.com>
2018-03-25 21:55:34 +0800

    add login components, add auth meta

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,19 +2,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import InventoryList from '@/components/InventoryList'
 import InventoryDetail from '@/components/InventoryDetail'
+import Login from '@/components/Login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'inventory:list',
+      name: 'inventory.list',
+      meta: {
+        auth: true
+      },
       component: InventoryList,
       children: [{
         path: '/inventory/:id',
-        name: 'inventory:detail',
+        name: 'inventory.detail',
         component: InventoryDetail,
       }]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {
+        auth: false
+      },
+      component: Login
     }
   ]
 })
