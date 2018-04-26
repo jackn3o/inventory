0890d5fb0871f93f1fd046378e266e1b7938e203
Jack Neoh <Jack Neoh@magicsoft-asia.com>
2018-04-26 17:45:42 +0800

    split to request_parser and response_writer for single responsive

---
+++ base/utilities/request_parser.go
@@ -0,0 +1,48 @@
+package utility
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func (u *Utility) parseJSON(value interface{}) (int, error) {
+	body, err := ioutil.ReadAll(u.Request.Body)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	if err := json.Unmarshal(body, &value); err != nil {
+		return http.StatusBadRequest, err
+	}
+	return http.StatusOK, nil
+}
+
+// Unmarshal is function to parse json from request header
+func (u *Utility) Unmarshal(value interface{}) (int, error) {
+	canonicalName := http.CanonicalHeaderKey("content-type")
+	headerValue := u.Request.Header.Get(canonicalName)
+	if headerValue != "" {
+		if strings.Contains(headerValue, "application/json") {
+			return u.parseJSON(value)
+		}
+	}
+	return http.StatusBadRequest, nil
+}
+
+// UnmarshalWithValidation is function to validate parse json from request header
+// Must have a validate function for the provide receiver
+func (u *Utility) UnmarshalWithValidation(value interface{}) error {
+	if _, err := u.Unmarshal(value); err != nil {
+		return err
+	}
+	govalidator.SetFieldsRequiredByDefault(true)
+	_, err := govalidator.ValidateStruct(value)
+	if err != nil {
+		println("error: " + err.Error())
+	}
+
+	return nil
+}
--- base/utilities/utility.go
+++ base/utilities/utility.go
@@ -1,20 +1,6 @@
 package utility
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
-type errorResponse struct {
-	Code    int         `json:"code,omitempty"`
-	Status  string      `json:"status,omitempty"`
-	Message interface{} `json:"message,omitempty"`
-}
-
-type jsonResponseDTO struct {
-	Data  interface{}   `json:"data,omitempty"`
-	Error errorResponse `json:"meta,omitempty"`
-}
+import "net/http"
 
 // Utility is common helper
 type Utility struct {
@@ -28,50 +14,4 @@ func New(writer http.ResponseWriter, req *http.Request) *Utility {
 		Writer:  writer,
 		Request: req,
 	}
-}
-
-// WriteJSON return JSON format data to client, default 200
-func (u *Utility) WriteJSON(value interface{}, vars ...int) error {
-	statusCode := http.StatusOK
-	if len(vars) > 0 {
-		statusCode = vars[0]
-	}
-	data, err := json.Marshal(jsonResponseDTO{
-		Data: value,
-	})
-
-	if err != nil {
-		http.Error(u.Writer, err.Error(), http.StatusInternalServerError)
-	} else {
-		u.Writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		if statusCode != http.StatusOK {
-			u.Writer.WriteHeader(statusCode)
-		}
-
-		u.Writer.Write(data)
-	}
-
-	return err
-}
-
-// WriteJSONError return JSON error to client
-func (u *Utility) WriteJSONError(value interface{}, statusCode int) {
-	errResponse, err := json.Marshal(jsonResponseDTO{
-		Error: errorResponse{
-			Code:    statusCode,
-			Status:  http.StatusText(statusCode),
-			Message: value,
-		},
-	})
-
-	if err != nil {
-		http.Error(u.Writer, err.Error(), http.StatusInternalServerError)
-	} else {
-		u.Writer.Write(errResponse)
-	}
-}
-
-// NotImplemented as a utitlies
-func (u *Utility) NotImplemented() {
-	u.Writer.Write([]byte("Not Implemented"))
 }
