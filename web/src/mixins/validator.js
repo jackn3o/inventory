d3d6f9f934deed6807be9cb9518d0e26f211e55f
Jack Neoh <Jack Neoh@magicsoft-asia.com>
2018-04-30 15:10:11 +0800

    add validator mixins

---
+++ web/src/mixins/validator.js
@@ -0,0 +1,76 @@
+const mixin = {
+  data() {
+    return {
+      errorMessage: '',
+      responseValidation: null
+    }
+  },
+  computed: {
+    showError() {
+      return this.errorMessage
+    },
+    validations() {
+      let obj_model = this.model,
+        obj_result = {}
+
+      for (let str_key in obj_model) {
+        obj_result[str_key] = []
+      }
+
+      if (!this.responseValidation) {
+        return obj_result
+      }
+
+      for (let str_key in obj_result) {
+        let str_validation = this.responseValidation[str_key]
+        if (str_validation) {
+          obj_result[str_key] = [str_validation]
+        }
+      }
+
+      return obj_result
+    }
+  },
+  methods: {
+    handleValidation(obj_error) {
+      console.log('validator mixin: start')
+
+      let obj_meta = obj_error.response.data.meta
+      if (!obj_meta) {
+        console.log('validator mixin: end, no meta')
+        return
+      }
+
+      let obj_validations = obj_meta.messages.validations
+      if (!obj_validations) {
+        console.log('validator mixin: end, no validation message')
+        return
+      }
+      this.responseValidation = obj_meta.messages.validations
+      console.log('validator mixin: complete')
+      return Promise.reject(obj_error)
+    },
+    resetValidation() {
+      this.errorMessage = ''
+      this.responseValidation = null
+    },
+    post(str_url, obj_data = null) {
+      this.resetValidation()
+
+      return this.axios({
+        method: 'post',
+        url: str_url,
+        data: obj_data
+      }).catch(obj_error => {
+        return this.handleValidation(obj_error)
+      })
+    }
+  },
+  created() {
+    if (!this.model) {
+      throw 'validator mixin: missing model'
+    }
+  }
+}
+
+export default mixin
